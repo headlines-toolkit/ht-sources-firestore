3fabbc3c884fd71f832b3cd0303cc9610097ee82
fulleni <fulleni@hotmail.com>
2025-03-30 08:19:41 +0100

    feat: initial commit for ht_sources_firebase
    - Added basic project structure
    - Created initial library files
    - Configured analysis options
    - Added github workflows

---
+++ lib/src/ht_sources_firebase.dart
@@ -0,0 +1,7 @@
+/// {@template ht_sources_firebase}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtSourcesFirebase {
+  /// {@macro ht_sources_firebase}
+  const HtSourcesFirebase();
+}
---
+++ test/src/ht_sources_firebase_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ht_sources_firebase/ht_sources_firebase.dart';
+
+void main() {
+  group('HtSourcesFirebase', () {
+    test('can be instantiated', () {
+      expect(HtSourcesFirebase(), isNotNull);
+    });
+  });
+}
